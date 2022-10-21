bd349b36fcc3591ffb47ce62175935139a14c9a2
npc <pheobemcpherson@gmail.com>
2022-10-21 18:56:11 +0500

    feat(v5): complete 'topics'

--- v5/receive_logs_topic/receive_logs_topic.go
+++ v5/receive_logs_topic/receive_logs_topic.go
@@ -23,13 +23,13 @@ func main() {
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"logs_direct", // name
-		"direct",      // type
-		true,          // durable
-		false,         // auto-deleted
-		false,         // internal
-		false,         // no-wait
-		nil,           // arguments
+		"logs_topic", // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
 
@@ -44,16 +44,16 @@ func main() {
 	failOnError(err, "Failed to declare a queue")
 
 	if len(os.Args) < 2 {
-		log.Printf("Usage: %s [info] [warning] [error]", os.Args[0])
+		log.Printf("Usage: %s [binding_key]...", os.Args[0])
 		os.Exit(0)
 	}
 	for _, s := range os.Args[1:] {
 		log.Printf("Binding queue %s to exchange %s with routing key %s",
-			q.Name, "logs_direct", s)
+			q.Name, "logs_topic", s)
 		err = ch.QueueBind(
-			q.Name,        // queue name
-			s,             // routing key
-			"logs_direct", // exchange
+			q.Name,       // queue name
+			s,            // routing key
+			"logs_topic", // exchange
 			false,
 			nil)
 		failOnError(err, "Failed to bind a queue")
@@ -62,10 +62,10 @@ func main() {
 	msgs, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
-		true,   // auto-ack
+		true,   // auto ack
 		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
+		false,  // no local
+		false,  // no wait
 		nil,    // args
 	)
 	failOnError(err, "Failed to register a consumer")
